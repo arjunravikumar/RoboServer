82e41c154d6a02204df57b13c24df8e6cbcca3d3
Arjun Ravikumar <54692274+arjunravikumar@users.noreply.github.com>
2021-09-06 21:53:02 -0400

    Update objectDetection.py

--- objectDetection.py
+++ objectDetection.py
@@ -2,6 +2,7 @@ import jetson.inference
 import jetson.utils
 from flask import Flask, render_template, Response, send_from_directory
 from flask_cors import *
+import cv2
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -10,10 +11,19 @@ net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.videoSource("http://192.168.1.166:5000/video_feed")      # '/dev/video0' for V4L2
 display = jetson.utils.videoOutput("file://detectionImg.jpg",true) # 'my_video.mp4' for file
 
-while display.IsStreaming():
-	img = camera.Capture()
-	detections = net.Detect(img)
-	for detection in detections:
-		print(detection)
-	display.Render(img)
-	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+def gen():
+	while True:
+		img = camera.Capture()
+		detections = net.Detect(img)
+		for detection in detections:
+			print(detection)
+		display.Render(img)
+		display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+		imgOut = jetson.utils.cudaToNumpy(img)
+		yeild(imgOut)
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
