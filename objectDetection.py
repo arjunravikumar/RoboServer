c4e337ba922f20cb94b41d35b08b60a2433feb53
ar4038 <a4038@ri.edu>
2021-09-07 21:17:14 -0400

    trying out some fixes

--- objectDetection.py
+++ objectDetection.py
@@ -1,7 +1,5 @@
 import jetson.inference
 import jetson.utils
-from flask import Flask, render_template, Response, send_from_directory
-from flask_cors import *
 import cv2
 
 app = Flask(__name__)
@@ -11,26 +9,11 @@ net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.videoSource("rtsp://192.168.1.166:8554/unicast")      # '/dev/video0' for V4L2
 display = jetson.utils.videoOutput("rtp://192.168.1.11:5000") # 'my_video.mp4' for file
 
-def gen():
-	while True:
-		img = camera.Capture()
-		detections = net.Detect(img)
-		for detection in detections:
-			print(detection)
-		display.Render(img)
-		display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
-		imgOut = jetson.utils.cudaToNumpy(img)
-		yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + imgOut + b'\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-app.run(debug=True)
+while True:
+    img = camera.Capture()
+    detections = net.Detect(img)
+    for detection in detections:
+        print(detection)
+    display.Render(img)
+    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+    imgOut = jetson.utils.cudaToNumpy(img)
