15edebcef039fa933262b8ff2728ce0ec682bb59
ar4038 <a4038@ri.edu>
2021-09-07 21:24:00 -0400

    trying out some fixes

--- objectDetection.py
+++ objectDetection.py
@@ -4,7 +4,7 @@ import cv2
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.videoSource("rtsp://192.168.1.166:8554/unicast")      # '/dev/video0' for V4L2
-display = jetson.utils.videoOutput("rtp://192.168.1.11:5000") # 'my_video.mp4' for file
+display = jetson.utils.videoOutput("rtp://192.168.1.11:5000","--headless") # 'my_video.mp4' for file
 
 while True:
     img = camera.Capture()
@@ -14,3 +14,5 @@ while True:
     display.Render(img)
     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
     imgOut = jetson.utils.cudaToNumpy(img)
+    if not camera.IsStreaming() or not display.IsStreaming():
+        break
