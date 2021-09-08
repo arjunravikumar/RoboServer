bb8d643bf693b9d936dcad721374458897d192dd
ar4038 <a4038@ri.edu>
2021-09-07 21:32:42 -0400

    trying out some fixes

--- objectDetection.py
+++ objectDetection.py
@@ -3,9 +3,8 @@ import jetson.utils
 import cv2
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = jetson.utils.videoSource("rtsp://192.168.1.166:8554/unicast")      # '/dev/video0' for V4L2
-display = jetson.utils.videoOutput("rtp://192.168.1.11:5000","--headless") # 'my_video.mp4' for file
-
+display = jetson.utils.videoOutput("rtp://192.168.1.11:5000","--headless")
+camera = jetson.utils.videoSource("rtsp://192.168.1.166:8554/unicast")
 while True:
     img = camera.Capture()
     detections = net.Detect(img)
