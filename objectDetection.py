02ad35b53e931e2ed50499cf2eb55c55141db9e1
ar4038 <a4038@ri.edu>
2021-09-08 23:31:25 -0400

    trying out some fixes

--- objectDetection.py
+++ objectDetection.py
@@ -1,12 +1,32 @@
 import jetson.inference
 import jetson.utils
 import cv2
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.videoSource("rtsp://192.168.1.166:8554/unicast")
 
-while True:
-    img = camera.Capture()
-    detections = net.Detect(img)
-    for detection in detections:
-        print(detection)
+
+def gen_frames():
+    while True:
+        img = camera.Capture()
+        detections = net.Detect(img)
+        for detection in detections:
+            print(detection)
+        ret, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
