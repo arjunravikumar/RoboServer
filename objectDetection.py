88c6eacfa34e100e89c84a6cdf86a26d26ed9840
ar4038 <a4038@ri.edu>
2021-09-08 22:52:39 -0400

    trying out some fixes

--- objectDetection.py
+++ objectDetection.py
@@ -4,7 +4,7 @@ import cv2
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.videoSource("rtsp://192.168.1.166:8554/unicast")
-display = jetson.utils.videoOutput("rtp://192.168.1.11:8554/test","--headless")
+display = jetson.utils.videoOutput("rtp://127.0.0.1:8554/test","--headless")
 
 while True:
     img = camera.Capture()
