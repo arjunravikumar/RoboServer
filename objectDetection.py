f7308b2b2de7fa8096f785db7445281bcf2cad53
ar4038 <a4038@ri.edu>
2021-09-10 10:15:32 -0400

    trying out some fixes

--- objectDetection.py
+++ objectDetection.py
@@ -17,24 +17,24 @@ def gen_frames():
         img = camera.Capture()
         detections = net.Detect(img)
         detectionsForImageTracking = []
-#         for detection in detections:
-#             detectedObjInImg = np.array([detection.Left,detection.Bottom,detection.Right,detection.Top,detection.ClassID])
-#             detectionsForImageTracking.append(detectionsForImageTracking)
-#         if(len(detectionsForImageTracking) > 0):
-#             detectionsForImageTracking = np.array(detectionsForImageTracking)
-#             tracked_objects = mot_tracker.update(detectionsForImageTracking)
-#             for x1, y1, x2, y2, obj_id, cls_pred in tracked_objects:
-#                 box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
-#                 box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
-#                 y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
-#                 x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
-#
-#                 color = colors[int(obj_id) % len(colors)]
-#                 color = [i * 255 for i in color]
-#                 cls = classes[int(cls_pred)]
-#                 cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
-#                 cv2.rectangle(frame, (x1, y1-35), (x1+len(cls)*19+60, y1), color, -1)
-#                 cv2.putText(frame, cls + "-" + str(int(obj_id)), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)
+        for detection in detections:
+            detectedObjInImg = np.array([detection.Left,detection.Bottom,detection.Right,detection.Top,detection.ClassID])
+            detectionsForImageTracking.append(detectedObjInImg)
+        if(len(detectionsForImageTracking) > 0):
+            detectionsForImageTracking = np.array(detectionsForImageTracking)
+            tracked_objects = mot_tracker.update(detectionsForImageTracking)
+            for x1, y1, x2, y2, obj_id, cls_pred in tracked_objects:
+                box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
+                box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
+                y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
+                x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
+
+                color = colors[int(obj_id) % len(colors)]
+                color = [i * 255 for i in color]
+                cls = classes[int(cls_pred)]
+                cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
+                cv2.rectangle(frame, (x1, y1-35), (x1+len(cls)*19+60, y1), color, -1)
+                cv2.putText(frame, cls + "-" + str(int(obj_id)), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)
 
         img_array = jetson.utils.cudaToNumpy(img)
         font                   = cv2.FONT_HERSHEY_SIMPLEX
