71fb17f86c43bc71044092055f1b4ff156e1d42a
ar4038 <a4038@ri.edu>
2021-09-17 13:28:47 -0400

    trying out some fixes

--- objectDetection.py
+++ objectDetection.py
@@ -3,34 +3,42 @@ import jetson.utils
 import cv2
 from flask import Flask, render_template, Response
 import numpy as np
+import threading
+import time
 
 app = Flask(__name__)
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.videoSource("rtsp://192.168.1.166:8554/unicast")
 tracker = None
+labelClasses = {}
+toDetect = "MIL"
+frame = None
 
-def createTracker(tracker_type):
+def createTracker(trackerType):
     global tracker
-    if tracker_type == 'BOOSTING':
+    if trackerType == 'BOOSTING':
         tracker = cv2.legacy.TrackerBoosting_create()
-    if tracker_type == 'MIL':
+    if trackerType == 'MIL':
         tracker = cv2.legacy.TrackerMIL_create()
-    if tracker_type == 'KCF':
+    if trackerType == 'KCF':
         tracker = cv2.legacy.TrackerKCF_create()
-    if tracker_type == 'TLD':
+    if trackerType == 'TLD':
         tracker = cv2.legacy.TrackerTLD_create()
-    if tracker_type == 'MEDIANFLOW':
+    if trackerType == 'MEDIANFLOW':
         tracker = cv2.legacy.TrackerMedianFlow_create()
-    if tracker_type == 'GOTURN':
+    if trackerType == 'GOTURN':
         tracker = cv2.legacy.TrackerGOTURN_create()
-    if tracker_type == 'MOSSE':
+    if trackerType == 'MOSSE':
         tracker = cv2.legacy.TrackerMOSSE_create()
-    if tracker_type == "CSRT":
+    if trackerType == "CSRT":
         tracker = cv2.legacy.TrackerCSRT_create()
 
 def gen_frames():
     global tracker
+    global frame
+    global conditionObj
+    global toDetect
     objectFound = False
     font                   = cv2.FONT_HERSHEY_SIMPLEX
     bottomLeftCornerOfText = (10,650)
@@ -46,27 +54,33 @@ def gen_frames():
             img_array = jetson.utils.cudaToNumpy(img)
             for detection in detections:
                 print(detection)
-                if(detection.ClassID == 1):
+                if(labelClasses[detection.ClassID]["className"] == toDetect):
                     detection = detections[0]
                     print(detection.Left+detection.Width,detection.Top+detection.Height)
-                    bbox = [int(detection.Left),int(detection.Top),int(detection.Width),int(detection.Height)]
-                    ok = tracker.init(img_array, bbox)
+                    boundingBox = [int(detection.Left),int(detection.Top),int(detection.Width),int(detection.Height)]
+                    ok = tracker.init(img_array, boundingBox)
                     objectFound = True
-        ok, bbox = tracker.update(img_array)
+        ok, boundingBox = tracker.update(img_array)
         if ok:
-            p1 = (int(bbox[0]), int(bbox[1]))
-            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            p1 = (int(boundingBox[0]), int(boundingBox[1]))
+            p2 = (int(boundingBox[0] + boundingBox[2]), int(boundingBox[1] + boundingBox[3]))
             cv2.rectangle(img_array, p1, p2, (255,0,0), 2, 1)
         else :
-            cv2.putText(img_array, "Tracking failure detected", (100,80), font, 0.75,(0,0,255),2)
+            cv2.putText(img_array, labelClasses[detection.ClassID]["className"] + "Not Visible in Vision", (20,20), font, 0.50,(0,0,255),2)
             objectFound = False
-        cv2.putText(img_array, "Tracking "+ str(detection.ClassID) , (100,20), font, 0.75, (50,170,50),2)
+        cv2.putText(img_array, "Tracking "+ labelClasses[detection.ClassID]["className"] , (20,80), font, 0.50, (50,170,50),2)
         cv2.putText(img_array,'FPS: '+str(net.GetNetworkFPS()), bottomLeftCornerOfText, font,fontScale,fontColor,lineType)
         ret, buffer = cv2.imencode('.jpg', img_array)
         frame = buffer.tobytes()
+        conditionObj.notifyAll()
+
+def getFrames():
+    global frame
+    global conditionObj
+    while True:
+        conditionObj.wait()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -75,5 +89,25 @@ def index():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-createTracker('BOOSTING')
-app.run("0.0.0.0",port="8000",debug=True)
+def initalisePreProcessingProcedure():
+    global labelClasses
+    createTracker('BOOSTING')
+    with open('label.txt','r') as f:
+        lines = f.readlines()
+        labelClasses = {}
+        for line in lines:
+            classVals = line.replace("\n","").split("\t")
+            labelClasses[classVals[0]] = {"className": classVals[1],"classCategory": classVals[4]}
+
+def startWebServer():
+    app.run("0.0.0.0",port="8000",debug=True)
+
+initalisePreProcessingProcedure()
+conditionObj = threading.Condition()
+gen_frames(toDetect)
+
+generateFrames = threading.Thread(target=gen_frames, name='generateFrames')
+webServer = threading.Thread(target=startWebServer, name='webServer')
+
+generateFrames.join()
+webServer.join()
