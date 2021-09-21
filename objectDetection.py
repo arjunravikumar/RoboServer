fd578d7aaafcaa8374704989fc8525fc5d09ceac
ar4038 <a4038@ri.edu>
2021-09-21 01:00:46 -0400

    bug fixes

--- objectDetection.py
+++ objectDetection.py
@@ -106,24 +106,6 @@ def gen_frames(toDetect):
         img_array = jetson.utils.cudaToNumpy(img)
         if(objectFound):
             trackSubjectUsingRobot(bBoxDetect)
-#         if(frameCount%100 == 0 or objectFound == False):
-#             printStatus("Detecting Object")
-#             objectFound, bBoxDetect, img = getDesiredObjectFromFrame(toDetect,img)
-#             printStatus("Object Detected"+str(objectFound))
-#             resetTracking = True
-#             frameCount = 1
-#         frameCount += 1
-#         img_array = jetson.utils.cudaToNumpy(img)
-#         if(resetTracking and objectFound):
-#             printStatus("Reset Initiated"+str(bBoxDetect))
-#             resetTracking = False
-#             tracker.init(img_array, bBoxDetect)
-#             printStatus("Tracking Initialised")
-#         if(objectFound):
-#             printStatus("Tracking Object")
-#             objectFound, bBoxTrack,img_array = trackObject(img_array,toDetect)
-#             trackSubjectUsingRobot(bBoxTrack)
-#             printStatus("Object Status"+str(objectFound))
         if(GUIMode):
             cv2.putText(img_array,'FPS: '+str(net.GetNetworkFPS()), (10,650), \
             cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2)
