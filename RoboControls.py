9924867df155845ecab9b5a7d42597c3a9c53e8e
ar4038 <a4038@ri.edu>
2021-09-20 22:52:33 -0400

    bug fixes

--- RoboControls.py
+++ RoboControls.py
@@ -1,40 +1,32 @@
-import websocket
 import time
+import asyncio
+import websockets
+
 
 class RoboControls:
-    ws = None
+    websocket = None
     robotIsMobile = False
+
     def __init__(self):
-        websocket.enableTrace(True)
-        self.ws = websocket.WebSocketApp("ws://192.168.1.166:8888",
-                                  on_message = self.on_message,
-                                  on_error = self.on_error,
-                                  on_close = self.on_close)
-        self.ws.on_open = self.on_open
-        self.ws.run_forever()
+        startWebSocket()
+        asyncio.get_event_loop().run_until_complete(startWebSocket())
 
-    def on_message(self,ws, message):
-        print(message)
+    async def startWebSocket():
+        async with websockets.connect('ws://192.168.1.166:8888') as self.websocket:
+            await self.websocket.send("tumbler:wakeup")
+            while(True):
+                msgRecv = await websocket.recv()
+                messageRecieved(msgRecv)
 
-    def on_error(self,ws, error):
-        print(error)
+    def messageRecieved(msgRecv):
+        print("message from server: ",msgRecv)
 
-    def on_close(self,ws,arg1=None,arg2=None):
-        print("### closed ###")
-
-    def closeWS(self):
-        self.ws.close()
-
-    def on_open(self,ws):
-        self.ws.send("tumbler:wakeup")
-        print("Connected")
-
-    def move(self,direction,speed=100):
+    async def move(self,direction,speed=100):
         if(self.robotIsMobile == False):
             self.robotIsMobile = True
-            self.ws.send(direction)
+            await self.websocket.send(direction)
 
-    def stopMovement(self):
+    async def stopMovement(self):
         if(self.robotIsMobile == True):
             self.robotIsMobile = False
-            self.ws.send("DS")
+            await self.websocket.send("DS")
