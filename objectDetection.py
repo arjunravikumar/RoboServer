e55131b962ff4e7171b002b8b55b289c8dfe3b54
ar4038 <a4038@ri.edu>
2021-09-20 20:14:41 -0400

    bug fixes

--- objectDetection.py
+++ objectDetection.py
@@ -76,6 +76,7 @@ def trackSubjectUsingRobot(bBoxTrack):
     global robotControls
     xMid,yMid = bBoxTrack[0]+(bBoxTrack[2]/2),bBoxTrack[1]+(bBoxTrack[3]/2)
     screenCenterX,screenCenterY = screenWidth/2,screenHeight/2
+    print(abs(xMid - screenCenterX) , screenWidth/4)
     if(abs(xMid - screenCenterX) > screenWidth/4):
         if(xMid>screenCenterX):
             print("right")
