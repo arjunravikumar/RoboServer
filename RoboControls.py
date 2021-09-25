09ac3f85cfc0c8e54b7100f9a63c2f576ae55868
ar4038 <a4038@ri.edu>
2021-09-25 16:12:38 -0400

    bug fixes

--- RoboControls.py
+++ RoboControls.py
@@ -9,12 +9,12 @@ class RoboControls:
 
     def startWS(self):
         websocket.enableTrace(True)
-#         self.ws = websocket.WebSocketApp("ws://192.168.1.166:8888",
-#                                   on_message = self.on_message,
-#                                   on_error = self.on_error,
-#                                   on_close = self.on_close)
-#         self.ws.on_open = self.on_open
-#         self.ws.run_forever()
+        self.ws = websocket.WebSocketApp("ws://192.168.1.166:8888",
+                                  on_message = self.on_message,
+                                  on_error = self.on_error,
+                                  on_close = self.on_close)
+        self.ws.on_open = self.on_open
+        self.ws.run_forever()
 
     def on_message(self,ws, message):
         print(message)
@@ -32,11 +32,11 @@ class RoboControls:
     def on_open(self,ws):
         def run(self):
             self.ws.send("tumbler:wakeup")
-#         _thread.start_new_thread(run, (self,))
+        _thread.start_new_thread(run, (self,))
 
     def send(self,message):
         def run(self):
             self.ws.send(json.dumps(message))
         if((message["direction"]+message["turn"]) != self.currentDirection):
             self.currentDirection = message["direction"]+message["turn"]
-#             _thread.start_new_thread(run, (self,))
+            _thread.start_new_thread(run, (self,))
