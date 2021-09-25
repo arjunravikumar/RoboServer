ff56274d0547b1e7b804d884a36bf392e3fd159e
ar4038 <a4038@ri.edu>
2021-09-25 15:06:59 -0400

    bug fixes

--- objectDetection.py
+++ objectDetection.py
@@ -85,7 +85,8 @@ def prepareMessageToSend(bBoxTrack):
     printStatus("bBoxTrack "+str(bBoxTrack))
     xMid,yMid = bBoxTrack[0]+(bBoxTrack[2]/2),bBoxTrack[1]+(bBoxTrack[3]/2)
     screenCenterX,screenCenterY = screenWidth/2,screenHeight/2
-    if(abs(xMid - screenCenterX) > (screenWidth/60)):
+    if(abs(xMid - screenCenterX) > (screenWidth/30)):
+        messageToSend["stopIn"] = (abs(xMid - screenCenterX)/1000)
         if(xMid > screenCenterX):
             printStatus("right " + str(xMid) + " " +str(screenCenterX))
             messageToSend["turn"] = "right"
