df093875f43cba72690a5925c6d484e4ddb51e03
ar4038 <a4038@ri.edu>
2021-09-25 21:33:53 -0400

    bug fixes

--- objectDetection.py
+++ objectDetection.py
@@ -90,7 +90,7 @@ def prepareMessageToSend(bBoxTrack):
     screenCenterX,screenCenterY = screenWidth/2,screenHeight/2
     printStatus("image latency "+str(latency))
     if(abs(xMid - screenCenterX) > (screenWidth/20)):
-        hyperParam = 0.1
+        hyperParam = 0.9
         latency = (hyperParam * latency) + ((1 - hyperParam) * (latency -0.0001))
         latency = max(latency,0)
         messageToSend["latency"] = latency
@@ -108,7 +108,7 @@ def prepareMessageToSend(bBoxTrack):
         printStatus("stop " + str(xMid) + " " +str(screenCenterX))
         messageToSend["direction"] = "stop"
         messageToSend["reason"] = "Object in center of Frame Increase latency"
-        hyperParam = 0.1
+        hyperParam = 0.9
         latency = (hyperParam * latency) + ((1 - hyperParam) * (latency + 0.1))
         latency = max(latency,0)
         messageToSend["latency"] = latency
