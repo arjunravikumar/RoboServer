c3d915164d226b379534c3c2f5015bd3e6cc76b4
ar4038 <a4038@ri.edu>
2021-10-02 14:18:54 -0400

    bug fixes

--- RoboControls.py
+++ RoboControls.py
@@ -7,7 +7,6 @@ class RoboControls:
     ws = None
 
     def startWS(self):
-        websocket.enableTrace(True)
         self.ws = websocket.WebSocketApp("ws://192.168.1.166:8888",
                                   on_message = self.on_message,
                                   on_error = self.on_error,
