bdb75a038f265e02ff419b43c2ebd1711d389ddb
ar4038 <a4038@ri.edu>
2021-10-02 15:40:58 -0400

    bug fixes

--- objectDetection.py
+++ objectDetection.py
@@ -120,7 +120,7 @@ def prepareMessageToSend(bBoxTrack):
             messageToSend["turn"] = "left"
             currentDirection = "left"
             return True, messageToSend
-    elif((abs(xGroundTruthPos - screenCenterX) < (screenWidth/20))\
+    elif((abs(xGroundTruthPos - screenCenterX) < (screenWidth/50))\
             and currentDirection!= "stop"):
         printStatus("stop " + "cameraPos "+ str(xMid) + " groundTruth "\
                                 + str(xGroundTruthPos) + " " +str(screenCenterX))
