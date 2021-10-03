f8fa77232878ed058967b48fe8dd5d0cd8b411c3
ar4038 <a4038@ri.edu>
2021-10-02 22:24:42 -0400

    bug fixes

--- objectDetection.py
+++ objectDetection.py
@@ -113,7 +113,7 @@ def prepareMessageToSend(bBoxTrack):
             + str(xMid) + " " +str(screenCenterX))
             messageToSend["turn"] = "right"
             currentDirection = "right"
-            start_time = threading.Timer(stopIn,stopOnCenter)
+            start_time = threading.Timer(stopIn,stopOnCenter,args=(xMid,yMid))
             start_time.start()
             return True, messageToSend
         elif(xMid < screenCenterX and currentDirection != "left"):
@@ -122,7 +122,7 @@ def prepareMessageToSend(bBoxTrack):
                         + str(xMid) + " " +str(screenCenterX))
             messageToSend["turn"] = "left"
             currentDirection = "left"
-            start_time = threading.Timer(stopIn,stopOnCenter)
+            start_time = threading.Timer(stopIn,stopOnCenter,args=(xMid,yMid))
             start_time.start()
             return True, messageToSend
     elif((abs(xMid - screenCenterX) < (screenWidth/50))\
@@ -137,7 +137,7 @@ def prepareMessageToSend(bBoxTrack):
         return True, messageToSend
     return False,None
 
-def stopOnCenter():
+def stopOnCenter(xMid,yMid):
     global robotControls, videoLatency, currentDirection, stopPos
     stopPos = [xMid,yMid]
     movementEndTime = time.time()
